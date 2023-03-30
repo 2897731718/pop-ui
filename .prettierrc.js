ef87c5655a5fc85513c9a9efac8e77af0e88af9e
曾书伟 <2897731718@qq.com>
2023-03-30 11:07:34 +0800

    dispose: eslint prettier husky

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: false, // 末尾逗号 默认为 true
+  singleQuote: true, // 单引号
+  trailingComma: 'none', // 末尾分号
+  arrowParens: 'avoid',
+  proseWrap: 'never',
+  endOfLine: 'auto',
+}
