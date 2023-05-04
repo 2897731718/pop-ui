95960506f240d0a91847c43b380f1a90fa6dc07f
曾书伟 <2897731718@qq.com>
2023-05-04 15:39:45 +0800

    feat: defaultButton

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Button from './button'
+
+createApp(App).use(Button).mount('#app')
